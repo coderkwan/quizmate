7b29d0c11d240c45a333c0e5157b8732fa500c7f
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2023-04-14 17:11:57 +0200

    added creator name

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,7 +2,10 @@ import styles from "../styles/Nav.module.css";
 export default function Index() {
     return (
         <div className={styles.footer_container}>
-            <small>Made for you, for fun.</small>
+            <p>
+                Made for you, for fun. By{" "}
+                <a href="https://kwanele.netlify.app">Kwanele Gamedze</a>
+            </p>
         </div>
     );
 }
